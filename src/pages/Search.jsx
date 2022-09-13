11c7f0ab7912ceaf53e30ed88acb2886a77c6c3a
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-13 01:46:48 -0300

    Req 5 done

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,38 @@ import { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchState: '',
+  };
+
+  handleSeachState = (event) => {
+    const { target: { value } } = event;
+    this.setState({
+      searchState: value,
+    });
+  };
+
   render() {
+    const { searchState } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+
+        <input
+          type="text"
+          placeholder="Pesquisar"
+          data-testid="search-artist-input"
+          value={ searchState }
+          name="search"
+          onChange={ this.handleSeachState }
+        />
+        <button
+          type="submit"
+          disabled={ searchState.length < 2 }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
