d25a7f107bf3b004d4cf6e2eb89ed8872fb92a2e
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-13 03:49:07 -0300

    Almost 1 hour spent to find out that test was failing cuz a SPACE on line 66. Report JEST fail logic test

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,15 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends Component {
   state = {
     searchState: '',
+    APIresponse: false,
+    songs: [],
+    albumFetched: '',
   };
 
   handleSeachState = (event) => {
@@ -13,27 +19,73 @@ class Search extends Component {
     });
   };
 
-  render() {
+  handleSearch = async () => {
     const { searchState } = this.state;
+    this.setState({
+      APIresponse: true,
+    }, async () => {
+      const songListFetch = await searchAlbumsAPI(searchState);
+      this.setState({
+        APIresponse: false,
+        songs: songListFetch,
+      });
+    });
+    this.setState({ albumFetched: searchState });
+    this.setState({ searchState: '' });
+  };
+
+  render() {
+    const { songs, searchState, APIresponse, albumFetched } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        { APIresponse ? <Loading /> : (
+          <>
+            <input
+              type="text"
+              placeholder="Pesquisar"
+              data-testid="search-artist-input"
+              value={ searchState }
+              name="search"
+              onChange={ this.handleSeachState }
+            />
+            <button
+              type="submit"
+              disabled={ searchState.length < 2 }
+              data-testid="search-artist-button"
+              onClick={ this.handleSearch }
+            >
+              Pesquisar
+            </button>
+          </>
+        )}
+        {songs.length ? (
+          <ol>
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              { albumFetched }
+            </p>
+            {songs.map((song) => (
+              <li key={ `${song.collectionId}` }>
+                <div>
+                  <h3>{`Álbum ${song.collectionName}`}</h3>
+                  <span>{ `Artista ${song.artistName}` }</span>
+                  <img src={ song.artworkUrl100 } alt={ song.artistName } />
+                  <Link
+                    to={ `/album/${song.collectionId}` }
+                    data-testid={ `link-to-album-${song.collectionId}` }
+                  >
+                    <br />
+                    Visualizar Álbum
+                  </Link>
+                </div>
+              </li>
+            ))}
 
-        <input
-          type="text"
-          placeholder="Pesquisar"
-          data-testid="search-artist-input"
-          value={ searchState }
-          name="search"
-          onChange={ this.handleSeachState }
-        />
-        <button
-          type="submit"
-          disabled={ searchState.length < 2 }
-          data-testid="search-artist-button"
-        >
-          Pesquisar
-        </button>
+          </ol>
+        ) : <p>Nenhum álbum foi encontrado</p>}
       </div>
     );
   }
